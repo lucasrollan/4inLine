a4804ecd99e0027573de09ad3259909ed47b2f75
Lucas Rollan <lrollan@medallia.com>
2016-03-27 17:48:20 -0300

    Just before streamline

--- src/index.js
+++ src/index.js
@@ -1,90 +1,16 @@
-var _ = require('underscore');
+var interface = require('./playModes');
 
-var playTree = require('./playTree');
-var findBestMove = require('./findBestMove');
-var readlineSync = require('readline-sync');
-var logBoard = require('./logBoard');
-var isWinningMove = require('./isWinningMove');
-var i18n = require('./i18n');
-var aiReport = require('./ai/report');
-var msg = aiReport.keys;
+var gameEnd = false;
 
-i18n.setLanguage('es');
+interface.setMode(interface.modes.TERMINAL);
+interface.action(interface.actions.GAME_START);
 
-var gameState = {
-    board: [
-        '',  //0
-        '',  //1
-        '',  //2
-        '',  //3
-        '',  //4
-        '',  //5
-        ''  //6
-    ],
-    playing: true,
-    players: ['HUMAN', 'AI'],
-    currentPlayer: null
-};
+while (!gameEnd) {
+    interface.action(interface.actions.TURN_START);
 
-logBoard(gameState.board);
+    var index = interface.action(interface.actions.TURN_INPUT);
+    interface.action(interface.actions.TURN_MOVEMENT_CHOSEN, index);
 
-var playing = true;
-var players = ['X', 'O'];
-var currentPlayer = _.sample(players);
-
-function printBoard() {
-    var print = logBoard(gameState.board);
-
-    print.forEach(function(line) {
-        console.log(line);
-    });
-
-    console.log('   1  2  3  4  5  6  7   ');
+    gameEnd = interface.action(interface.actions.TURN_END, index);
 }
-
-function turn() {
-    printBoard();
-
-    var index = playerInput[currentPlayer]();
-
-    gameState.board[index] += currentPlayer;
-
-    if (isWinningMove(gameState.board, index)) {
-        playing = false;
-    } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    }
-}
-
-var playerInput = {
-    'X': function AI_turn(){
-        console.log('>>>', i18n.getString(i18n.keys.TURN_AI)(), '<<<');
-        aiReport.log(msg.AI_THINKING);
-
-        var bestMove = playTree(gameState.board, 'X', 6);
-
-        aiReport.log(msg.AI_CHOSEN_COLUMN, {column: bestMove + 1});
-
-        return bestMove;
-    },
-    'O': function human_turn() {
-        console.log('>>>', i18n.getString(i18n.keys.TURN_HUMAN)(), '<<<');
-        var options = [1, 2, 3, 4, 5, 6, 7];
-
-        var index = readlineSync.keyInSelect(
-            options, i18n.getString(i18n.keys.SELECT_COLUMN)(),
-            {
-                cancel: false
-            }
-        );
-
-        return index;
-    }
-};
-
-while (playing) {
-    turn();
-}
-printBoard();
-console.log('>', i18n.getString(i18n.keys.GAME_OVER)());
-console.log('>', i18n.getString(i18n.keys.WINNER_PLAYER)({player: currentPlayer}));
+interface.action(interface.actions.GAME_END);
